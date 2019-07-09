b7af204c5d8b6a53b4121e623b37b498525151d1
Ibbus93 <ibbus93@gmail.com>
2019-07-09 10:51:23 +0200

    Added new users data to edit in the exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,13 @@ import {GoogleAnalyticsService} from './google-analytics.service';
 export class AppComponent implements OnInit {
 
   sessioni = {};
+  nuoviUtenti = {};
 
   constructor(private servizioGoogle: GoogleAnalyticsService) {}
 
   async ngOnInit() {
     await this.caricaSessioniGoogle();
+    await this.caricaNuoviUtentiGoogle();
   }
 
   async caricaSessioniGoogle() {
@@ -41,5 +43,34 @@ export class AppComponent implements OnInit {
       console.error(e);
     }
   }
+
+  async caricaNuoviUtentiGoogle() {
+    const header = [['Data', 'Nuovi Utenti']];
+    let datiGoogle;
+
+    try {
+      datiGoogle = await this.servizioGoogle.recuperaSessioni().toPromise();
+      datiGoogle = datiGoogle.map(el => [new Date(el[0]), el[1]]);
+      datiGoogle = header.concat(datiGoogle);
+
+      this.nuoviUtenti = {
+        chartType: 'AreaChart',
+        dataTable: datiGoogle,
+        options: {
+          height: 400,
+          colors: ['blue'],
+          legend: {
+            position: 'bottom'
+          }
+        }
+      };
+
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+
+
 
 }
